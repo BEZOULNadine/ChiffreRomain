2c96e29ace50fdcad82e95ed65c6c3d82433cd10
BEZOULNadine <bezoulnadine25@gmail.com>
2026-04-23 08:34:20 +0000

    first

---
+++ src/Chiffreromain/Game.java
@@ -0,0 +1,5 @@
+package Chiffreromain;
+
+public class Game {
+
+}
---
+++ test/Chiffreromain/GameTest.java
@@ -0,0 +1,21 @@
+package Chiffreromain;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+	Game Gm;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Gm = new Game();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
